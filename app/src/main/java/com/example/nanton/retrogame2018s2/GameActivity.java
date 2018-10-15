8cf752d2d7abd060f3fcdefb22756764e29ca5e3
Weinan Zhang <jovany.zane@anu.edu.au>
2018-10-16 00:42:29 +1100

    update the map of the game activity

--- app/src/main/java/com/example/nanton/retrogame2018s2/GameActivity.java
+++ app/src/main/java/com/example/nanton/retrogame2018s2/GameActivity.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
-
+    private Drawing drawingView;
+    static GameActivity activity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        drawingView = new Drawing(this);
+        setContentView(drawingView);
+        activity=this;
+
     }
 }
