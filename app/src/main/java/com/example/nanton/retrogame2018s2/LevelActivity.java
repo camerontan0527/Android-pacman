c5bd489100835de4111ba80543cc089ada030761
Weinan Zhang <jovany.zane@anu.edu.au>
2018-10-16 00:11:05 +1100

    Acitivity design and implement the function

---
+++ app/src/main/java/com/example/nanton/retrogame2018s2/LevelActivity.java
@@ -0,0 +1,13 @@
+package com.example.nanton.retrogame2018s2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level);
+    }
+}
--- app/src/main/java/com/example/nanton/retrogame2018s2/MainActivity.java
+++ app/src/main/java/com/example/nanton/retrogame2018s2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.nanton.retrogame2018s2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+    public void StartGame(View view){
+        Intent start = new Intent(this, GameActivity.class);
+        startActivity(start);
+    }
+    public void Level(View view){
+        Intent start = new Intent(this, GameActivity.class);
+        startActivity(start);
+    }
+    public void About(View view){
+        Intent start = new Intent(this, GameActivity.class);
+        startActivity(start);
+    }
+    }
+
