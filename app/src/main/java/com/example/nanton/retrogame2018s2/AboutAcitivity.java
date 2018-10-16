ebdfbd3669626b1d75ab49a38e9f9f17286b9a1c
Weinan Zhang <jovany.zane@anu.edu.au>
2018-10-16 22:25:19 +1100

    background music setting finished.

--- app/src/main/java/com/example/nanton/retrogame2018s2/AboutAcitivity.java
+++ app/src/main/java/com/example/nanton/retrogame2018s2/AboutAcitivity.java
@@ -1,13 +1,19 @@
 package com.example.nanton.retrogame2018s2;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AboutAcitivity extends AppCompatActivity {
 
+    private static MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_acitivity);
+        mediaPlayer=MediaPlayer.create(this,R.raw.pacmanmusic);
+        mediaPlayer.setVolume(100,100);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
     }
 }
