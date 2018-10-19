70d771942950ee96034533260f2f68c3655be232
00ox_xo00 <u6302766@anu.edu.au>
2018-10-19 11:41:10 +1100

    Re-write map, add coins, add monster, move monster

--- app/src/main/java/com/example/nanton/retrogame2018s2/GameActivity.java
+++ app/src/main/java/com/example/nanton/retrogame2018s2/GameActivity.java
@@ -1,142 +1,113 @@
 package com.example.nanton.retrogame2018s2;
 
 import android.graphics.Color;
+import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class GameActivity extends AppCompatActivity {
+    public enum Direction{ UP,DOWN,RIGHT,LEFT}
+    Direction monsDirect = Direction.DOWN;
+    Direction pacDirect = Direction.RIGHT;
+
     ConstraintLayout ly;
-    ArrayList<TextView> map = new ArrayList();
+    int[][] myMap;
+    int blockSize;
+
+    ImageView monster;
+    int monsX = 13, monsY = 4;
+
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         ly = findViewById(R.id.myLayout);
-        initLines();
-        drawLines();
-
+        monster = findViewById(R.id.monster);
+        newMap();
+        handler.postDelayed(myRunnable, 200);
     }
 
-    int numOfLines = 26; // modify number
-    int height = 50; // modify number
-    int width = 300; // modify number
-    static private int[] xs = {0,
-            300,
-            600,
-            900,
-            1200,
-            0,
-            0,
-            0,
-            250,
-            350,
-            450,
-            300,
-            500,
-            1200,
-            900,
-            600,
-            300,
-            0,
-            0,
-            300,
-            50,
-            600,
-            350,
-            0,
-            300,
-            600
+    Runnable myRunnable = new Runnable() {
+        @Override
+        public void run() {
+            updateUI();
+            handler.postDelayed(myRunnable, 200);
+        }
+    };
+    private void updateUI(){
+        clearCoins();
+        drawCoins();
+        moveMonster();
+    }
+    private void moveMonster(){
+        if(monsDirect==Direction.DOWN){
+            if(myMap[monsX][monsY+1] == -1){
+                monsDirect = Direction.UP;
+                monsY = monsY - 1;
+            }else{
+                monsY = monsY + 1;
+            }
+        }else if(monsDirect==Direction.UP){
+            if(myMap[monsX][monsY-1] == -1){
+                monsDirect = Direction.DOWN;
+                monsY = monsY + 1;
+            }else{
+                monsY = monsY - 1;
+            }
+        }
+        monster.setX(blockSize*monsX);
+        monster.setY(blockSize*monsY);
+    }
 
-
-
-
-    }; // modify number
-    static private int[] ys = {0,
-            0,
-            0,
-            0,
-            0,
-            150,
-            200,
-            0,
-            0,
-            0,
-            0,
-            350,
-            650,
-            1400,
-            1400,
-            1400,
-            1400,
-            1400,
-            1400,
-            1400,
-            350,
-            350,
-            50,
-            900,
-            900,
-            900
-
-
-    }; // modify number
-
-    static private int[] rots = {0,
-            0,
-            0,
-            0,
-            0,
-            90,
-            90,
-            90,
-            90,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            90,
-            0,
-            0,
-            0
-
-
-
-
-    }; // modify number
-
-    private void initLines() {
-        for (int i = 0; i < numOfLines; i++) {
-            TextView tv = new TextView(this);
-            tv.setBackgroundColor(Color.BLUE);
-            tv.setHeight(height);
-            tv.setWidth(width);
-            ly.addView(tv);
-            map.add(tv);
+    ArrayList<TextView> coins = new ArrayList();
+    private void clearCoins(){
+        for(TextView tv : coins){
+            ly.removeView(tv);
+        }
+        coins.clear();
+    }
+    private void drawCoins(){
+        for(int i = 0; i < Value.dividedSize; i++){
+            for(int j = 0; j < Value.dividedSize; j++){
+                if(myMap[i][j] == 1){
+                    TextView tv = new TextView(this);
+                    tv.setBackgroundColor(Color.WHITE);
+                    tv.setHeight(10);
+                    tv.setWidth(10);
+                    tv.setX(blockSize*i + blockSize / 2 - 5);
+                    tv.setY(blockSize*j + blockSize / 2 - 5);
+                    coins.add(tv);
+                    ly.addView(tv);
+                }
+            }
         }
     }
 
-    private void drawLines() {
-        for (int i = 0; i < numOfLines; i++) {
-            TextView tv = map.get(i);
-            tv.setX(xs[i]);
-            tv.setY(ys[i]);
-            tv.setRotation(rots[i]);
+    private void newMap(){
+        myMap = Value.map.clone();
+        blockSize = Value.screenWidth/Value.dividedSize;
+        for(int i = 0; i < Value.dividedSize; i++){
+            for(int j = 0; j < Value.dividedSize; j++){
+                if(myMap[i][j] == -1){
+                    TextView tv = new TextView(this);
+                    tv.setBackgroundColor(Color.WHITE);
+                    tv.setHeight(blockSize);
+                    tv.setWidth(blockSize);
+                    tv.setX(blockSize*i);
+                    tv.setY(blockSize*j);
+                    ly.addView(tv);
+                }
+            }
         }
-
+        monster.setX(blockSize*monsX);
+        monster.setY(blockSize*monsY);
     }
 
 
--- app/src/main/java/com/example/nanton/retrogame2018s2/MainActivity.java
+++ app/src/main/java/com/example/nanton/retrogame2018s2/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        Value.screenWidth = metrics.widthPixels;
         mediaPlayer = MediaPlayer.create(this, R.raw.pacmanmusic);
         mediaPlayer.setVolume(100, 100);
         mediaPlayer.setLooping(true);
