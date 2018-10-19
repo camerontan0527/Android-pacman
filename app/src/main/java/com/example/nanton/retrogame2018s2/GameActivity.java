74ed30da604077fe0dfe4e2c8dd07431bf76607d
00ox_xo00 <u6302766@anu.edu.au>
2018-10-19 12:47:25 +1100

    add score, move button, pacman, movable pacman

--- app/src/main/java/com/example/nanton/retrogame2018s2/GameActivity.java
+++ app/src/main/java/com/example/nanton/retrogame2018s2/GameActivity.java
@@ -5,22 +5,28 @@ import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class GameActivity extends AppCompatActivity {
     public enum Direction{ UP,DOWN,RIGHT,LEFT}
     Direction monsDirect = Direction.DOWN;
-    Direction pacDirect = Direction.RIGHT;
+    Direction pacDirect = null;
 
     ConstraintLayout ly;
     int[][] myMap;
     int blockSize;
 
+    TextView score;
+    int scoreValue = 0;
     ImageView monster;
     int monsX = 13, monsY = 4;
+    ImageView pacman;
+    int pacX = Value.dividedSize-2, pacY = Value.dividedSize-2;
+    HashMap<String,TextView> coins = new HashMap<>();
 
     Handler handler = new Handler();
 
@@ -30,6 +36,8 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         ly = findViewById(R.id.myLayout);
         monster = findViewById(R.id.monster);
+        pacman = findViewById(R.id.pacman);
+        score = findViewById(R.id.score);
         newMap();
         handler.postDelayed(myRunnable, 200);
     }
@@ -42,9 +50,69 @@ public class GameActivity extends AppCompatActivity {
         }
     };
     private void updateUI(){
-        clearCoins();
-        drawCoins();
         moveMonster();
+        movePacman();
+    }
+    private void movePacman(){
+        if(pacDirect == null){
+            return;
+        }else if(pacDirect == Direction.RIGHT){
+            if(myMap[pacX+1][pacY] == -1){
+                return;
+            }else{
+                pacX++;
+                pacman.setX(pacX*blockSize);pacman.setY(pacY*blockSize);
+                if(myMap[pacX][pacY] == 1){
+                    myMap[pacX][pacY] = 0;
+                    ly.removeView(coins.get(""+pacX+" "+pacY));
+                    coins.remove(""+pacX+" "+pacY);
+                    scoreValue = scoreValue + 10;
+                }
+                score.setText(""+scoreValue);
+            }
+        }else if(pacDirect == Direction.LEFT){
+            if(myMap[pacX-1][pacY] == -1){
+                return;
+            }else{
+                pacX--;
+                pacman.setX(pacX*blockSize);pacman.setY(pacY*blockSize);
+                if(myMap[pacX][pacY] == 1){
+                    myMap[pacX][pacY] = 0;
+                    ly.removeView(coins.get(""+pacX+" "+pacY));
+                    coins.remove(""+pacX+" "+pacY);
+                    scoreValue = scoreValue + 10;
+                }
+                score.setText(""+scoreValue);
+            }
+        }else if(pacDirect == Direction.UP){
+            if(myMap[pacX][pacY-1] == -1){
+                return;
+            }else{
+                pacY--;
+                pacman.setX(pacX*blockSize);pacman.setY(pacY*blockSize);
+                if(myMap[pacX][pacY] == 1){
+                    myMap[pacX][pacY] = 0;
+                    ly.removeView(coins.get(""+pacX+" "+pacY));
+                    coins.remove(""+pacX+" "+pacY);
+                    scoreValue = scoreValue + 10;
+                }
+                score.setText(""+scoreValue);
+            }
+        }else if(pacDirect == Direction.DOWN){
+            if(myMap[pacX][pacY+1] == -1){
+                return;
+            }else{
+                pacY++;
+                pacman.setX(pacX*blockSize);pacman.setY(pacY*blockSize);
+                if(myMap[pacX][pacY] == 1){
+                    myMap[pacX][pacY] = 0;
+                    ly.removeView(coins.get(""+pacX+" "+pacY));
+                    coins.remove(""+pacX+" "+pacY);
+                    scoreValue = scoreValue + 10;
+                }
+                score.setText(""+scoreValue);
+            }
+        }
     }
     private void moveMonster(){
         if(monsDirect==Direction.DOWN){
@@ -66,36 +134,15 @@ public class GameActivity extends AppCompatActivity {
         monster.setY(blockSize*monsY);
     }
 
-    ArrayList<TextView> coins = new ArrayList();
-    private void clearCoins(){
-        for(TextView tv : coins){
-            ly.removeView(tv);
-        }
-        coins.clear();
-    }
-    private void drawCoins(){
-        for(int i = 0; i < Value.dividedSize; i++){
-            for(int j = 0; j < Value.dividedSize; j++){
-                if(myMap[i][j] == 1){
-                    TextView tv = new TextView(this);
-                    tv.setBackgroundColor(Color.WHITE);
-                    tv.setHeight(10);
-                    tv.setWidth(10);
-                    tv.setX(blockSize*i + blockSize / 2 - 5);
-                    tv.setY(blockSize*j + blockSize / 2 - 5);
-                    coins.add(tv);
-                    ly.addView(tv);
-                }
-            }
-        }
-    }
 
     private void newMap(){
         myMap = Value.map.clone();
         blockSize = Value.screenWidth/Value.dividedSize;
+
+        // add wall and coins
         for(int i = 0; i < Value.dividedSize; i++){
             for(int j = 0; j < Value.dividedSize; j++){
-                if(myMap[i][j] == -1){
+                if(myMap[i][j] == -1){ // add wall
                     TextView tv = new TextView(this);
                     tv.setBackgroundColor(Color.WHITE);
                     tv.setHeight(blockSize);
@@ -103,14 +150,43 @@ public class GameActivity extends AppCompatActivity {
                     tv.setX(blockSize*i);
                     tv.setY(blockSize*j);
                     ly.addView(tv);
+                }else if(myMap[i][j] == 1){ // add coins
+                    TextView tv = new TextView(this);
+                    tv.setBackgroundColor(Color.WHITE);
+                    tv.setHeight(10);
+                    tv.setWidth(10);
+                    tv.setX(blockSize*i + blockSize / 2 - 5);
+                    tv.setY(blockSize*j + blockSize / 2 - 5);
+                    coins.put(""+i+" "+j,tv);
+                    ly.addView(tv);
                 }
             }
         }
+
+        // add monster
         monster.setX(blockSize*monsX);
         monster.setY(blockSize*monsY);
+
+        // add pacman
+        pacman.setX(blockSize*pacX);
+        pacman.setY(blockSize*pacY);
+
+        // set score
+        score.setText(""+scoreValue);
     }
 
-
+    public void clickLeft(View v){
+        pacDirect = Direction.LEFT;
+    }
+    public void clickRight(View v){
+        pacDirect = Direction.RIGHT;
+    }
+    public void clickUp(View v){
+        pacDirect = Direction.UP;
+    }
+    public void clickDown(View v){
+        pacDirect = Direction.DOWN;
+    }
 }
 
 
